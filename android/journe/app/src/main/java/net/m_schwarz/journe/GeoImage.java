aa01f8f0364a01703fb8685477144189594095e5
michael_schwarz <m.schwarz@tum.de>
2016-11-05 14:40:10 -0400

    Image preview

--- android/journe/app/src/main/java/net/m_schwarz/journe/GeoImage.java
+++ android/journe/app/src/main/java/net/m_schwarz/journe/GeoImage.java
@@ -4,4 +4,9 @@ package net.m_schwarz.journe;
  * Created by michael on 05.11.16.
  */
 public class GeoImage {
+    String url;
+
+    public GeoImage(String url){
+        this.url = url;
+    }
 }
--- android/journe/app/src/main/java/net/m_schwarz/journe/ImageAdapter.java
+++ android/journe/app/src/main/java/net/m_schwarz/journe/ImageAdapter.java
@@ -1,11 +1,17 @@
 package net.m_schwarz.journe;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+
+import java.io.InputStream;
 
 /**
  * Created by michael on 05.11.16.
@@ -29,7 +35,41 @@ public class ImageAdapter extends ArrayAdapter<GeoImage> {
         View rowView = inflater.inflate(R.layout.image_item, parent, false);
 
         ImageView imageView = (ImageView) rowView.findViewById(R.id.icon);
-
+        if(BitmapCache.contains(values[position].url)){
+            imageView.setImageBitmap(BitmapCache.get(values[position].url));
+        } else{
+            if(true || !BitmapCache.inTemptative(values[position].url)){
+                BitmapCache.putTemptative(values[position].url);
+                new ImageDownloader(imageView).execute(values[position].url);
+            }
+        }
         return rowView;
     }
+
+    class ImageDownloader extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+
+        public ImageDownloader(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String url = urls[0];
+            Bitmap mIcon = null;
+            try {
+                InputStream in = new java.net.URL(url).openStream();
+                mIcon = BitmapFactory.decodeStream(in);
+                mIcon = Bitmap.createScaledBitmap(mIcon,mIcon.getWidth()/2,mIcon.getHeight()/2,true);
+                BitmapCache.put(urls[0],mIcon);
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+            }
+            return mIcon;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            bmImage.setImageBitmap(result);
+        }
+    }
 }
+
