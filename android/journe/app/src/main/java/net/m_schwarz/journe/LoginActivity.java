8a7f28624df7bb9c35ecf50db6f9c0278d23051c
Sarah F <sarahfor25@hotmail.com>
2016-11-05 11:10:54 -0400

    Adding login/registration restrictions - valid email and matching passwords required

--- android/journe/app/src/main/java/net/m_schwarz/journe/LoginActivity.java
+++ android/journe/app/src/main/java/net/m_schwarz/journe/LoginActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -20,5 +22,13 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void clickLogin(View view) {
+        EditText et = (EditText) findViewById(R.id.username);
+        String email = et.getText().toString().trim();
+        String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
+        if (!email.matches(emailPattern)) {
+            Toast.makeText(getApplicationContext(),"Invalid email address", Toast.LENGTH_SHORT).show();
+            return;
+        }
     }
 }
--- android/journe/app/src/main/java/net/m_schwarz/journe/RegisterActivity.java
+++ android/journe/app/src/main/java/net/m_schwarz/journe/RegisterActivity.java
@@ -3,7 +3,11 @@ package net.m_schwarz.journe;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -23,6 +27,26 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
     public void clickLogin(View view) {
+
+        EditText et = (EditText) findViewById(R.id.username);
+        String email = et.getText().toString().trim();
+        String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
+        if (!email.matches(emailPattern)) {
+            Toast.makeText(getApplicationContext(),"Invalid email address", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        EditText pass = (EditText) findViewById(R.id.password);
+        EditText confirmPass = (EditText) findViewById(R.id.password_repeat);
+        String password = pass.getText().toString().trim();
+        String confirmPassword = confirmPass.getText().toString().trim();
+
+        if (!password.equals(confirmPassword)) {
+            Toast.makeText(getApplicationContext(),"Passwords do not match. Please re-enter.",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
         Intent intent = new Intent(this,LoginActivity.class);
         finish();
         startActivity(intent);
