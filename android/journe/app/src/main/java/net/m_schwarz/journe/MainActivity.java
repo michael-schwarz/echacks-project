e0e8aded2f611cad2834b2c497d9e0078512ac7b
michael_schwarz <m.schwarz@tum.de>
2016-11-05 10:33:52 -0400

    Prepare for geolocation

--- android/journe/app/src/main/java/net/m_schwarz/journe/MainActivity.java
+++ android/journe/app/src/main/java/net/m_schwarz/journe/MainActivity.java
@@ -1,7 +1,10 @@
 package net.m_schwarz.journe;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -151,6 +154,32 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
+    /**
+     * @return the last know best location
+     */
+    private Location getLastBestLocation() {
+        LocationManager mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        Location locationGPS = mLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        Location locationNet = mLocationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
+        long GPSLocationTime = 0;
+        if (null != locationGPS) { GPSLocationTime = locationGPS.getTime(); }
+
+        long NetLocationTime = 0;
+
+        if (null != locationNet) {
+            NetLocationTime = locationNet.getTime();
+        }
+
+        if ( 0 < GPSLocationTime - NetLocationTime ) {
+            return locationGPS;
+        }
+        else {
+            return locationNet;
+        }
+    }
+
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_TAKE_PHOTO && resultCode == Activity.RESULT_OK) {
@@ -160,7 +189,7 @@ public class MainActivity extends AppCompatActivity
                 protected Void doInBackground(Void... params) {
                     try {
                         ImageUpload img = new ImageUpload();
-                        img.doIt(new File(mCurrentPhotoPath));
+                        img.doIt(new File(mCurrentPhotoPath),getLastBestLocation());
                     } catch (Exception e) {
                         e.printStackTrace();
                     }
